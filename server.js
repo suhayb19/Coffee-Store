55df0896f95e46a52bab4df8f3dd71bce6d31b84
fearthebeard88 <tferris88@gmail.com>
2019-08-14 19:09:13 -0400

    added express server, moved static content to public directory, created routes for each page

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const path = require("path");
+
+app.use(express.json({extended: false}));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes middleware
+app.use("/home", require("./routers/home.js"));
+app.use("/news", require("./routers/news.js"));
+app.use("/contact", require("./routers/contact.js"));
+app.use("/about", require("./routers/about.js"));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
